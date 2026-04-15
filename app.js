fe355d4a16bab349d8db748b0feaf64e9e22f2c2
damien welch <nomad2142@protonmail.com>
2026-04-15 15:56:21 -0700

    added new constructor and new instances for it.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let products = [];
+let musicList = [];
 
 let images = document.querySelectorAll(".carousel:first-of-type .carousel-img");
+let musicImages = document.querySelectorAll(".carousel:last-of-type img");
+
 
 function Product(name, filepath) {
   this.name = name;
@@ -9,21 +12,37 @@ function Product(name, filepath) {
   products.push(this);
 }
 
-// Products
-new Product("Warframe", "assets/warframe.jpg");
-new Product("batman", "assets/batman.jpg");
-new Product("dyinglight", "assets/dyinglight.jpg");
-new Product("helldivers", "assets/helldivers.jpg");
-new Product("cyberpunk", "assets/cyberpunk.jpg");
-new Product("fallout", "assets/fallout.jpg");
-new Product("rimworld", "assets/rimworld.jpg");
-new Product("zomboid", "assets/zomboid.jpg");
-new Product("gta", "assets/gta.jpg");
-new Product("rdr2", "assets/rdr2.jpg");
-new Product("skyrim", "assets/skyrim.jpg");
+function Music(name, filepath) {
+  this.name = name;
+  this.filepath = filepath;
 
-function getRandomIndex() {
-  return Math.floor(Math.random() * products.length);
+  musicList.push(this);
+}
+
+
+new Product("Warframe", "assets/games/warframe.jpg");
+new Product("batman", "assets/games/batman.jpg");
+new Product("Dying Light", "assets/games/dyinglight.jpg");
+new Product("Helldivers", "assets/games/helldivers.jpg");
+new Product("Cyberpunk", "assets/games/cyberpunk.jpg");
+new Product("Fallout", "assets/games/fallout.jpg");
+new Product("Rimworld", "assets/games/rimworld.jpg");
+new Product("Zomboid", "assets/games/zomboid.jpg");
+new Product("Grand Theft Auto V", "assets/games/gta.jpg");
+new Product("Red Dead Redemption 2", "assets/games/rdr2.jpg");
+new Product("Skyrim", "assets/games/skyrim.jpg");
+
+new Music ("2009 Reggie Bush - $uicideboy$", "assets/music/thywillbedone.png")
+new Music ("Clouds as Witnesses - $uicideboy$", "assets/music/cloudsaswitnesses.jpg")
+new Music ("Euphoria - Kendrick Lamar", "assets/music/euphoria.jpg")
+new Music ("Enter Sandman - Metallica", "assets/music/entersandman.jpg")
+new Music ("Eulogy - $uicideboy$", "assets/music/eulogy.jpg")
+new Music ("COUNT YOUR BLESSINGS - $uicideboy$", "assets/music/thykingdomcome.jpg")
+new Music ("Truth - Seether", "assets/music/truth.jpg")
+new Music ("Rockstar In His Prime - Juice WRLD", "assets/music/rockstar.jpg")
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
 }
 
 function renderImages() {
@@ -33,7 +52,7 @@ function renderImages() {
     let randomIndex;
 
     do {
-      randomIndex = getRandomIndex();
+      randomIndex = getRandomIndex(products);
     } while (usedIndexes.includes(randomIndex));
 
     usedIndexes.push(randomIndex);
@@ -41,6 +60,22 @@ function renderImages() {
   }
 }
 
+function renderMusic() {
+  let usedIndexes = [];
+
+  for (let i = 0; i < musicImages.length; i++) {
+    let randomIndex;
+
+    do {
+      randomIndex = getRandomIndex(musicList);
+    } while (usedIndexes.includes(randomIndex));
+
+    usedIndexes.push(randomIndex);
+    musicImages[i].src = musicList[randomIndex].filepath;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   renderImages();
+  renderMusic();
 });
