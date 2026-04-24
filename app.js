0388ffc349ddd902de3c3eb81fbaeb4f3e62c6fb
damien welch <nomad2142@protonmail.com>
2026-04-24 15:21:10 -0700

    added new constructor property css selection and html hover card

--- app.js
+++ app.js
@@ -13,23 +13,23 @@ let images = document.querySelectorAll(".carousel:first-of-type .carousel-img");
 let musicImages = document.querySelectorAll(".carousel:last-of-type img");
 
 
-function Product(name, filepath) {
+function Product(name, filepath, description) {
   this.name = name;
   this.filepath = filepath;
-
+  this.description = description
   products.push(this);
 }
 
-function Music(name, filepath) {
+function Music(name, filepath, description) {
   this.name = name;
   this.filepath = filepath;
-
+  this.description
   musicList.push(this);
 }
 
 
-new Product("Warframe", "assets/games/warframe.jpg");
-new Product("batman", "assets/games/batman.jpg");
+new Product("Warframe", "assets/games/warframe.jpg", "Wareframe is a 3rd person looter shooter");
+new Product("Batman: Arkham Knight", "assets/games/batman.jpg", "Batman");
 new Product("Dying Light", "assets/games/dyinglight.jpg");
 new Product("Helldivers", "assets/games/helldivers.jpg");
 new Product("Cyberpunk", "assets/games/cyberpunk.jpg");
@@ -48,6 +48,8 @@ new Music ("Eulogy - $uicideboy$", "assets/music/eulogy.jpg")
 new Music ("COUNT YOUR BLESSINGS - $uicideboy$", "assets/music/thykingdomcome.jpg")
 new Music ("Truth - Seether", "assets/music/truth.jpg")
 new Music ("Rockstar In His Prime - Juice WRLD", "assets/music/rockstar.jpg")
+new Music ("Dis $ide - GODHANDUSA","assets/music/disside.jpg")
+new Music ("Judgement - Kensuke Ushio","assets/music/judgement.jpg")
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -109,13 +111,16 @@ function renderMusic() {
   previousMusicIndexes = [index1, index2, index3];
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
   renderImages();
   renderMusic();
-
+  // img.addEventListener
   gamesLeft.addEventListener("click", renderImages);
   gamesRight.addEventListener("click", renderImages);
 
   musicLeft.addEventListener("click", renderMusic);
   musicRight.addEventListener("click", renderMusic);
-});
+// });
